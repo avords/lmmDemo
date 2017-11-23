599f1f66ce2982efcb275aa61a10c7771b89866b
yandongquan <yandongquan@lvmama.com>
2017-11-23 17:46:41 +0800

    将spring boot打成war包放到tomcat 8中

---
+++ springBootDemo/src/main/java/com/lmm/mvc/controller/SampleServletInitializer.java
@@ -0,0 +1,14 @@
+package com.lmm.mvc.controller;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+/**
+ * Created by Administrator on 2017/11/23.
+ */
+public class SampleServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SampleController.class);
+    }
+}

3c41da8733cdadcd39bd839d3730f868b27421fa
yandongquan <yandongquan@lvmama.com>
2017-12-04 14:29:28 +0800

    增加jdbc的例子

--- springAopDemo/src/main/java/com/lmm/mvc/demo/service/ProductService.java
+++ springAopDemo/src/main/java/com/lmm/mvc/demo/service/ProductService.java
@@ -2,10 +2,13 @@ package com.lmm.mvc.demo.service;
 
 import com.lmm.mvc.demo.model.ProdProduct;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/11/25.
  */
 public interface ProductService {
     ProdProduct getByProductId(Long productId);
     int updateNameByProductId(String productName,Long productId);
+    List<ProdProduct> findProductsByIds(List<Long> productIds);
 }

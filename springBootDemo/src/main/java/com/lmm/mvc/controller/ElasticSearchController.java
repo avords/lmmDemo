c340070cb9ee99fa9b5e992b606094f3721aa4d4
arno.yan <arno.yan@lvmama.com>
2018-05-31 18:53:31 +0800

    fix

--- springBootDemo/src/main/java/com/lmm/mvc/controller/ElasticSearchController.java
+++ springBootDemo/src/main/java/com/lmm/mvc/controller/ElasticSearchController.java
@@ -4,13 +4,15 @@ import com.lmm.data.Article;
 import com.lmm.data.ArticleSearchRepository;
 import com.lmm.data.Author;
 import com.lmm.data.Tutorial;
-import org.elasticsearch.index.query.QueryStringQueryBuilder;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
-import java.util.Iterator;
 
 @RestController
 public class ElasticSearchController {
@@ -45,13 +47,31 @@ public class ElasticSearchController {
     }
 
     @RequestMapping("/query")
-    public void testSearch() {
+    public Page testSearch() {
+        /*// 分数，并自动按分排序
+        FunctionScoreQueryBuilder functionScoreQueryBuilder = QueryBuilders.functionScoreQuery()
+                .add(QueryBuilders.boolQuery().should(QueryBuilders.matchQuery("name", q)),
+                        ScoreFunctionBuilders.weightFactorFunction(1000)) // 权重：name 1000分
+                .add(QueryBuilders.boolQuery().should(QueryBuilders.matchQuery("message", q)),
+                        ScoreFunctionBuilders.weightFactorFunction(100)); // 权重：message 100分
+
+        // 分数、分页
+        SearchQuery searchQuery = new NativeSearchQueryBuilder().withPageable(pageable)
+                .withQuery(functionScoreQueryBuilder).build();*/
+
+        //QueryBuilders.boolQuery().must(QueryBuilders.termQuery()).must()
+        //must并且  should或者
         String queryString = "springboot";//搜索关键字
-        QueryStringQueryBuilder builder = new QueryStringQueryBuilder(queryString);
-        Iterable<Article> searchResult = articleSearchRepository.search(builder);
-        Iterator<Article> iterator = searchResult.iterator();
-        while (iterator.hasNext()) {
-            System.out.println(iterator.next());
-        }
+        
+        //QueryBuilders.matchQuery("","") 走分词器
+        //QueryBuilders.boolQuery() 多个条件拼接
+        //QueryBuilders.termQuery("","") 精确匹配
+
+        BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
+        queryBuilder.must(QueryBuilders.termQuery("id",1L));
+        queryBuilder.must(QueryBuilders.matchQuery("content", queryString));
+        
+        Page<Article> searchResult = articleSearchRepository.search(queryBuilder, PageRequest.of(0,5));
+        return searchResult;
     }
 }

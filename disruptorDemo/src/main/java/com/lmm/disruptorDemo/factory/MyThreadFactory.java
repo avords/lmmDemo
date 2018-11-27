d4cfd54f0848d144e22855197570ca8168dae76c
arno.yan <arno.yan@lvmama.com>
2018-11-27 11:26:15 +0800

    fix

--- disruptorDemo/src/main/java/com/lmm/disruptorDemo/factory/MyThreadFactory.java
+++ disruptorDemo/src/main/java/com/lmm/disruptorDemo/factory/MyThreadFactory.java
@@ -1,9 +1,11 @@
 package com.lmm.disruptorDemo.factory;
 
+import java.util.concurrent.ThreadFactory;
+
 /**
  * Created by arno.yan on 2018/11/27.
  */
-public class MyThreadFactory implements java.util.concurrent.ThreadFactory {
+public class MyThreadFactory implements ThreadFactory {
     @Override
     public Thread newThread(Runnable r) {
         return new Thread(r);

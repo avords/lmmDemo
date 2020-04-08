f451d308d896340667bc4bd5fe051794cd3acf03
arno.yan <arno.yan@lvmama.com>
2020-04-08 18:50:54 +0800

    fix

--- klHaveMoney/src/main/java/com/kl/quartz/Test.java
+++ klHaveMoney/src/main/java/com/kl/quartz/Test.java
@@ -12,7 +12,7 @@ public class Test {
 
     public static void main(String[] args) throws UnsupportedEncodingException {
         Base64 base64 = new Base64(true);
-        byte[] bytes = base64.decode("ESeh2iGA5n0mc7h9kkah5aTUobz%2BkCIlwEkqLvExVrzR1InxdPp%2FW6OkUdqjltj%2F");
-        System.out.println(new String(bytes,"utf-8"));
+        byte[] bytes = base64.decode("gytwVZo5sVX9UDMUEDnZ6iLerk2cNDpY4+EOeLDi9aRIdb3yIJHgP4R97iXKamGTn+KjcFJ4zRJAUAK6aw8MWeHbbOCXjksjJrmcbVKamtZILYAABc46lMjKAXZfeJxt");
+        System.out.println(new String(bytes,"gbk"));
     }
 }
--- springBootSecurityDemo/src/main/java/com/lmm/SecurityApplication.java
+++ springBootSecurityDemo/src/main/java/com/lmm/SecurityApplication.java
@@ -3,7 +3,7 @@ package com.lmm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.lmm"})
 public class SecurityApplication {
 
 	public static void main(String[] args) {

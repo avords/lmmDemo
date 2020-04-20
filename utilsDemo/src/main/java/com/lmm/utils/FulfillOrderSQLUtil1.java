05b368e3cb7351bdce9196b6a6c0d10198b656b1
arno.yan <arno.yan@lvmama.com>
2020-04-20 17:20:28 +0800

    FIX

--- utilsDemo/src/main/java/com/lmm/utils/FulfillOrderSQLUtil1.java
+++ utilsDemo/src/main/java/com/lmm/utils/FulfillOrderSQLUtil1.java
@@ -20,7 +20,7 @@ public class FulfillOrderSQLUtil1 {
                 "ALTER TABLE XIMA_TRD_{dbIndex}.TRD_PRICED_ORDER_{tbIndex} DROP INDEX IX_BUYER_ID;\n" +
                 "ALTER TABLE XIMA_TRD_{dbIndex}.TRD_PRICED_ORDER_LINE_{tbIndex} DROP INDEX IX_PRICED_ORDER_ID;";
         
-        sql = "CREATE TABLE `XIMA_FRD_{dbIndex}`.`FRD_FULFILL_ORDER_{tbIndex}` LIKE `XIMA_FRD_00`.`FRD_FULFILL_ORDER_00`;";
+        sql = "CREATE TABLE `XIMA_FRD_{dbIndex}`.`FRD_FULFILL_MASTER_ORDER_{tbIndex}` LIKE `XIMA_FRD_{dbIndex}`.`FRD_FULFILL_MASTER_ORDER_00`;";
         //sql = "UPDATE XIMA_TRD_{dbIndex}.TRD_ORDER_STATUS_RECORD_{tbIndex} SET STATUS_ID=2 WHERE STATUS_ID=4;";
         
         int tableNum = 100;
